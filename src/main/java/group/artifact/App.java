66c9a89f779cd808739d2a40daa3c060cf689a45
salaxy <salaxy@web.de>
2016-10-28 16:32:50 +0200

    - add ciphers, but does not work

--- src/main/java/group/artifact/App.java
+++ src/main/java/group/artifact/App.java
@@ -1,6 +1,7 @@
 package group.artifact;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.SSLContext;
@@ -10,9 +11,12 @@ import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.CipherSuite;
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.TlsVersion;
 
 /**
  * This is a try to figure out how it is possible to use Apple Provider API with
@@ -36,7 +40,7 @@ public class App {
 
 		System.out.println("Start Apple Push Test!");
 
-		final OkHttpClient client = App.getUnsafeOkHttpClient();
+		final OkHttpClient client = App.getSafeClient();
 		String adress = "https://" + HOST + ":" + PORT;
 		System.out.println(adress);
 		final Request request = new Request.Builder().url(adress).build();
@@ -44,7 +48,7 @@ public class App {
 		// The Http2Server should be running here.
 		final long startTime = System.nanoTime();
 
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 1; i++) {
 			Thread.sleep(1000);
 			// http://stackoverflow.com/questions/32625035/when-using-http2-in-okhttp-why-multi-requests-to-the-same-host-didnt-use-just
 			client.newCall(request).enqueue(new Callback() {
@@ -83,4 +87,31 @@ public class App {
 		}
 	}
 
+	// see@ https://github.com/square/okhttp/wiki/HTTPS
+	private static OkHttpClient getSafeClient() {
+
+		try {
+			// Install the all-trusting trust manager
+			final SSLContext sslContext = SSLContext.getInstance("SSL");
+			sslContext.init(null, SslContextFactory.TRUST_ALL_CERTS, new java.security.SecureRandom());
+			// Create an ssl socket factory with our all-trusting manager
+			// final SSLSocketFactory sslSocketFactory =
+			// sslContext.getSocketFactory();
+
+			ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS).tlsVersions(TlsVersion.TLS_1_2)
+					.cipherSuites(CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+							CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+							CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
+							CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256)
+					.build();
+
+			OkHttpClient okHttpClient = new OkHttpClient.Builder().connectionSpecs(Collections.singletonList(spec))
+					.build();
+			okHttpClient.sslSocketFactory().createSocket();
+
+			return okHttpClient;
+		} catch (final Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
